c47264707fb735ed49571eaaf206c6bb6ae35eb1
Thai Minh Nguyen <109969513+thaiminh0911@users.noreply.github.com>
2026-01-18 11:24:27 +0000

    Commit files

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+sudo sysctl -w vm.nr_hugepages=4096
+echo "vm.nr_hugepages=4096" | sudo tee -a /etc/sysctl.conf
+
+docker build -t xmrig .
+
 docker run -d \
   --name xmrig \
   --cap-add=IPC_LOCK \
   -v /workspaces/temp/config.json:/home/miner/config/config.json \
-  xmrig:6.25.0
+  xmrig:latest
